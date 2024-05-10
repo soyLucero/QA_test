8f4fa4a93195f84c26b5489f41e01aa9a15ae904
soyLucero <queen.luceroaccostupa@gmail.com>
2024-05-10 07:17:58 -0500

    segundo movimiento

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
+
+        System.out.println("HOLA LIMA_PERU");
         for(int i=100; i<=130; i=i+2){
             //System.out.println("Hola: " + i);
         }
